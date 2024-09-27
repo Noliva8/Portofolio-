982357ac827fa9ea5f7c2db28a23a14e3bd38160
Olivier Ndicunguye <nolivanoly@gmail.com>
2024-09-26 18:12:01 -0700

    netlify

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { Modal } from "react-bootstrap";
-import "./Contact.css";
+import "./contact.css";
 
 const Contact = () => {
   const form = useRef();
